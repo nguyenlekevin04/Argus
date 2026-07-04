7fb476bc3929045cef0c36dfa24f0741bb4ca4bb
Kevin Le Nguyen <s88877@stud.htw-dresden.de>
2026-07-04 17:13:03 +0200

    feat: return frames from camera

--- server/core/detection/capture.py
+++ server/core/detection/capture.py
@@ -1,16 +1,10 @@
 import cv2
 
-camera = cv2.VideoCapture(0)
-
-while camera.isOpened():
+def get_frame(camera):
+    """
+    Read a frame from the camera.
+    """
     ret, frame = camera.read()
     if not ret:
-        break
-
-    cv2.imshow('Camera Feed', frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-camera.release()
-cv2.destroyAllWindows()
+        raise ValueError("Failed to capture frame from camera.")
+    return frame
